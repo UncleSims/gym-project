2c549541a1ed69e9e82ffc106222fef5174fd84f
Ayodele Similoluwa <similoluwaayoayodele@gmail.com>
2023-07-02 20:49:27 +0100

    .

--- src/components/ExerciseCards.jsx
+++ src/components/ExerciseCards.jsx
@@ -7,12 +7,15 @@ const ExerciseCards = ({ exercise }) => {
       className="flex justify-between flex-col pb-2.5 scale-100 transition-all ease-in-out hover:scale-125  w-[400px] h-[430px] bg-white border-t-4 border-t-[rgb(255,38,37)] rounded-bl-md"
       to={`/exercise/${exercise.id}`}
     >
-      <img
-        className="h-[326px]"
-        src={exercise.gifUrl}
-        alt={exercise.name}
-        loading="lazy"
-      />
+      <div className=" relative h-[326px]">
+        <img
+          className="h-full object-cover w-screen"
+          src={exercise.gifUrl}
+          alt={exercise.name}
+          loading="lazy"
+        />
+      </div>
+
       <div className="flex ">
         <button className="ml-5 p-2 bg-[#ffa9a9] text-sm rounded-2xl capitalize text-white ">
           {exercise.bodyPart}
@@ -21,7 +24,9 @@ const ExerciseCards = ({ exercise }) => {
           {exercise.target}
         </button>
       </div>
-      <h3 className="ml-5 text-[#000] font-bold mt-2.5 pb-2.5 capitalize">{exercise.name}</h3>
+      <h3 className="ml-5 text-[#000] font-bold mt-2.5 pb-2.5 capitalize">
+        {exercise.name}
+      </h3>
     </Link>
   );
 };
